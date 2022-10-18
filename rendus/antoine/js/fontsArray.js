206d17f989cce1902393ed81c0f64ae05a765869
Antoine Metayer <antoinemetayer@MacBook-Pro-de-Antoine.local>
2022-10-18 14:29:49 +0200

    Add antoine

---
+++ rendus/antoine/js/fontsArray.js
@@ -0,0 +1,8 @@
+const fontsArray = [
+  [0, 'lemonregular'],
+  [1, 'acari_sansblack'],
+  [2, 'clickuperbold'],
+  [3, 'cramatenregular'],
+  [4, 'cursive_serifbook'],
+  [5, 'echotopiaregular']
+]
